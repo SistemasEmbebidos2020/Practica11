de673e509822ba9ab4cd02f9b649784cfbdcc486
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-28 19:28:53 -0500

    Add files via upload

---
+++ servidorRaspi.py
@@ -0,0 +1,71 @@
+import os
+import socket
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import RPi.GPIO as GPIO
+from time import*
+
+led1 = 20
+led2 = 21
+
+
+print(socket.gethostbyname(socket.gethostname()))
+ 
+def peripheral_setup():
+ GPIO.setmode(GPIO.BCM)
+ GPIO.setup(led1, GPIO.OUT)
+ GPIO.setup(led2, GPIO.OUT)
+ 
+ 
+def servidor():
+ Request = None
+
+ class RequestHandler_httpd(BaseHTTPRequestHandler):
+   def do_GET(self):
+    global Request
+
+    messagetosend = bytes('Solicitando',"utf")
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/plain')
+    self.send_header('Content-Length', len(messagetosend))
+    self.end_headers()
+    self.wfile.write(messagetosend)
+    Request = self.requestline
+    Request = Request[5 : int(len(Request)-9)]
+    #print(Request)
+    if Request == 'on1':
+      my_callback1()
+      print('bomba1 encendida')
+      GPIO.output(led1,True)
+      GPIO.output(led2,False)
+      
+    if Request == 'on2':
+      my_callback2()
+      print('bomba2 encendida')
+      GPIO.output(led2,True)
+      GPIO.output(led1,False)
+      
+    
+ server_address_httpd = ('192.168.0.128',8001)
+ httpd = HTTPServer(server_address_httpd, RequestHandler_httpd)
+ print('conectando a servidor')
+ print(httpd.fileno())
+ httpd.serve_forever()
+ 
+ 
+ 
+def main () :
+
+# Setup
+ peripheral_setup()
+
+# Infinite loop
+ try:
+  while 1 :  
+   servidor()
+ except(KeyboardInterrupt,SystemExit):
+  print ("BYE")
+  GPIO.cleanup()
+# Command line execution
+if __name__ == '__main__' :
+   main()
---
+++ subirAfirebase.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+from time import*
+from firebase import firebase
+
+firebase = firebase.FirebaseApplication('https://raspi1-embebidos-default-rtdb.firebaseio.com/', None)
+
+bajo1 = 22
+medio1 = 27
+
+def nivelbajo1(self):
+ firebase.put("/Tanque1", "/bajo1", GPIO.input(bajo1))
+
+def nivelmedio1(self):
+ firebase.put("/Tanque1", "/medio1", GPIO.input(medio1))
+
+
+def peripheral_setup():
+ GPIO.setmode(GPIO.BCM)
+ GPIO.setup(bajo1, GPIO.IN, GPIO.PUD_DOWN)
+ GPIO.setup(medio1, GPIO.IN, GPIO.PUD_DOWN)
+
+ GPIO.add_event_detect(bajo1,GPIO.BOTH,nivelbajo1,bouncetime=30)
+ GPIO.add_event_detect(medio1,GPIO.BOTH,nivelmedio1,bouncetime=30)
+
+
+
+def main () :
+
+# Setup
+ peripheral_setup()
+
+# Infinite loop
+ try:
+  while 1 :  
+   pass
+ except(KeyboardInterrupt,SystemExit):
+  print ("BYE")
+  GPIO.cleanup()
+# Command line execution
+if __name__ == '__main__' :
+   main()
